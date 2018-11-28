71d8cbb85156d7299e5454089f50cccb78582a26
vladimirjv <ivladimirjuarez@outlook.com>
2018-11-27 20:26:12 -0600

    adding files for heroku

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-@app.route('/hello')
+@app.route('/')
 def hello_world():
     return 'Hello, World!'
 
@@ -12,7 +12,7 @@ class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(HelloWorld, '/hello')
 
 if __name__ == '__main__':
     app.run(debug=True)
