30f1d80929276a88084c8be1967b03ad967d78c4
vladimirjv <ivladimirjuarez@outlook.com>
2018-11-29 08:34:26 -0600

    again adding the appi endpint

--- app.py
+++ app.py
@@ -24,17 +24,17 @@ class TestArray(Resource):
         # put('http://127.0.0.1:5000/test', data={'list': '[23,23,1243]'}).json()
         return { 'valores':lista }
 
-# class Compensar(Resource):
-#     def put(self):
-#         frq=json.loads(request.form['frecuencia'])
-#         # pk=json.loads(request.form['pk'])
-#         compensacion=EvaluarBS(frq)
-#         return {'compensacion' : compensacion }
+class Compensar(Resource):
+    def put(self):
+        frq=json.loads(request.form['frecuencia'])
+        # pk=json.loads(request.form['pk'])
+        compensacion=EvaluarBS(frq)
+        return {'compensacion' : compensacion }
 
 
 api.add_resource(HelloWorld, '/hello')
 api.add_resource(TestArray,'/test')
-# api.add_resource(Compensar, '/compensacion')
+api.add_resource(Compensar, '/compensacion')
 
 if __name__ == '__main__':
     app.run(debug=False)
