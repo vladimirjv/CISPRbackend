575f95097b741f10a7be3feaeeb763a9ff63d3ec
vladimirjv <ivladimirjuarez@outlook.com>
2018-12-02 14:38:22 -0600

    updat

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import json
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_cors import CORS
 from CalculationCl import MyMath
 import numpy as np
 # from calculation import Test
 math=MyMath()
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 @app.route('/')
