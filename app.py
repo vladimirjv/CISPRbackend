91b07134ab160d98cbff1612444ab6a47ac2a5a1
vladimirjv <ivladimirjuarez@outlook.com>
2018-12-04 07:31:22 -0600

    changes on server

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 import json
+import sys
 from flask import Flask, request
-from flask_restful import Resource, Api
-from flask_cors import CORS
+from flask_restful import Resource, Api, reqparse
+from flask_cors import CORS, cross_origin
 from CalculationCl import MyMath
 import numpy as np
 # from calculation import Test
 math=MyMath()
 
 app = Flask(__name__)
-CORS(app)
+cors = CORS(app)
 api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('lista')
+parser.add_argument('db')
+
 
 @app.route('/')
 def hello_world():
@@ -21,22 +27,36 @@ class HelloWorld(Resource):
 
 class TestArray(Resource):
     def put(self):
-        lista=json.loads(request.form['lista'])
-        # frq = np.array(json.loads(request.form['frecuencia']))
-        # db = np.array(json.loads(request.form['db']))
-        # val=math.interpolPkSemiFar(frq)
-        # return {'values': (val).tolist(), 'db': (db-val).tolist() }
-        return {'lista':lista}
+        frq = np.array(json.loads(request.form['frecuencia']))
+        db = np.array(json.loads(request.form['db']))
+        val = math.interpolPkSemiFar(frq)
+        return {'values': (val).tolist(), 'db': (db-val).tolist()}
+        # return {'values': (frq).tolist(), 'db': (db).tolist()}
     def get(self):
         frq=[]
         return { 'valores': frq }
 
 class CompararPkSemi(Resource):
+    support_cors = True
+    cors_origin = '*'
+    cors_headers = 'origin, content-type, x-request-with'
     def put(self):
-        frq = np.array(json.loads(request.form['frecuencia']))
-        db = np.array(json.loads(request.form['db']))
-        val = math.interpolPkSemiFar(frq)
-        return {'values': (val).tolist(), 'db': (db-val).tolist()}
+        args = parser.parse_args()
+        frq = args['lista']
+        db = args['db']
+        frq = list(map(int, frq.split(",")))
+        pk = list(map(int, db.split(",")))
+        # frqA=np.array([30,45,80,180])
+        frqA=np.array(frq)
+        pkA=np.array(pk)
+        val=math.interpolPkSemiFar(frqA)
+        # val = (val).tolist()}
+        # return {'status': 'ok'}
+        return {'frecuencia': frq, 'pk': pk, 'realPk': (val).tolist()}
+    def get(self):
+        frq=self.frq
+        pk=self.pk
+        return {'frecuencia': frq, 'pk':pk, 'realPk': ''}
 
 class DatosSemiFar(Resource):
     def get(self):
